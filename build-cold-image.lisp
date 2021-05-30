575538189bf6ea95ec1c3348c8e8de468ad98fcf
Sylvia Harrington <sylvia.harrington28@gmail.com>
2021-05-30 21:38:19 +0100

    Update name & submodules

--- build-cold-image.lisp
+++ build-cold-image.lisp
@@ -1,5 +1,4 @@
-;;;; Copyright (c) 2016 Henry Harrington <henry.harrington@gmail.com>
-;;;; This code is licensed under the MIT license.
+;;;; Build the cold image
 
 #-quicklisp
 (error "Quicklisp not found!")
--- run-file-server.lisp
+++ run-file-server.lisp
@@ -1,5 +1,4 @@
-;;;; Copyright (c) 2016 Henry Harrington <henry.harrington@gmail.com>
-;;;; This code is licensed under the MIT license.
+;;;; Load and start the file server.
 
 #-quicklisp
 (error "Quicklisp not found!")
